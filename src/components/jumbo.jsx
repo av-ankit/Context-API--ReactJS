2fd2212e2b3cf6398dce376fa6565221f58bb8a8
Ankit Verma <63111601+av-ankit@users.noreply.github.com>
2021-02-24 10:54:49 +0530

    Add files via upload

---
+++ src/components/jumbo.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./../App.css";
+import { Jumbotron,Button } from "react-bootstrap";
+import Listo from './extraDetail';
+import FewDetail from './fewDetail'
+class Jumbo extends Component {
+
+  constructor()
+  {
+    super();
+    this.state ={
+      
+      showExtraDetails : false,
+    }
+  }
+
+  toggle =()=>{
+    this.setState({showExtraDetails:!this.state.showExtraDetails,})
+  }
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <h1>Product {this.state.showExtraDetails?`Details`:`List`}</h1>
+          <div>
+            {
+            this.state.showExtraDetails ?
+            <Listo/>
+            :
+            <FewDetail/>
+            }
+            </div>
+            
+
+            <div>
+              <Button variant='outline-warning' onClick={this.toggle}>{this.state.showExtraDetails?`Less Details`:`Check Stock`}</Button>
+            </div>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default Jumbo;
